2985b69f9c73c102f59ba9eaa3605ad148c22e6a
Damian Oguche <damianguch@yahoo.com>
2024-03-09 04:54:59 +0100

    modified user_input.py

--- user_input.py
+++ user_input.py
@@ -2,4 +2,4 @@ name = input("What is your name? ")
 age = input("How old are you? ")
 location = input("Where is your location? ")
 
-print("Hello " + name, "you are " + age + " years old and live in " + location +".")
+print(f"Hello  {name}, you are {age} years old and live in  {location}")
